a03a30cb69d471814687ac1000447e086ea737ae
Ahmedtayeb-62 <ahmedtayeb62@gmail.com>
2025-07-24 22:55:44 +0300

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createHashRouter } from "react-router-dom";
 import { About } from "./Components/About/About.jsx";
 import Home from "./Components/Home/Home.jsx";
 import Error from "./Components/Error/Error.jsx";
@@ -8,7 +8,7 @@ import { Portfolio } from "./Components/Portfolio/Portfolio.jsx";
 import { Contact } from "./Components/Contact/Contact.jsx";
 
 function App() {
-  const router = createBrowserRouter([
+  const router = createHashRouter([
     {
       path: "/Start-Framework",
       element: <Layout />,
